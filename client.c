a1fe9a9f2100439431cbca94f3d02e2012a7b535
dune-official <baierl@dhbw-loerrach.de>
2023-03-06 18:15:34 +0100

    Update client.c
    - To send the datagram, the script now uses a struct to keep the datagram fields named. This eliminates the need for a transmission length
    - All "strncmp" and "strncpy" have been replaced by the corresponding "mem" alternative to increase performance and circumvent the bug that occurs when one of the six bytes in a hardware address are zero, so strncmp / strncpy stop the loop
    - The possibility to use (and expand) command line arguments has been added: mac_address; interface_name

--- client.c
+++ client.c
@@ -12,8 +12,6 @@
 #include <netinet/ether.h>
 #include <net/ethernet.h>
 
-#define DEF_IF "eth0"
-
 #define HARDWARE_ETHERNET_LOCATION_PROTOCOL 0x9001
 #define HELP 0x9001
 
@@ -23,18 +21,52 @@
 #define MODE_BROADREPLY 0x3
 
 #define MODE_LEN 1 
-#define PKG_ID_LEN 2
+#define PKT_ID_LEN 2
 #define NETW_ID_LEN 4
 
 /* function prototypes */
+void process_arguments(int argc, char *argv[], char *default_interface, unsigned char *default_dest_mac);
 void print_mac(const unsigned char *mac_address, FILE *loc);
 char char_to_hex(char input);
-unsigned char *string_to_mac(const char *string);
-unsigned char *create_packet(const char *restrict destination_mac, struct ifreq source_mac, const char *restrict network_id, unsigned char *restrict mode, const char *restrict packet_id, uint_fast16_t *restrict tx_len);
-void send_packet(int socket_fd, const unsigned char *packet, const uint_fast16_t *tx_len, const unsigned char *destination_mac, struct ifreq interface_ID);
+void string_to_mac(const char *string, unsigned char *dest_mac);
+unsigned char *create_packet(const char *restrict destination_mac, struct ifreq source_mac, const char *restrict network_id, unsigned char *restrict mode, const char *restrict packet_id);
+void send_packet(int socket_fd, const unsigned char *packet, const unsigned char *destination_mac, struct ifreq interface_ID);
 void *wait_for_packet(void *thread_argument);
 
 /*************************************** Befinning of file ********************************/
+
+typedef struct EtherPing {
+  char network_id[NETW_ID_LEN];
+  char mode;
+  char packet_id[PKT_ID_LEN];
+} EtherPing;
+
+void process_arguments(int argc, char *argv[], char *default_interface, unsigned char *default_dest_mac) {
+
+  int i;
+  for (i = 1; i < argc; i++) {
+    if (!memcmp(argv[i], "-i", 2) || !memcmp(argv[i], "--interface", 11)) {
+
+      if (i+1 == argc) {
+        fputs("\t[\033[31mERROR\033[0m] Could not find interface in command line argument\n", stderr);
+        exit(1);
+      }
+      memcpy(default_interface, argv[i+1], IFNAMSIZ);
+
+    } else if (!memcmp(argv[i], "-m", 2) || !memcmp(argv[i], "--mac", 5)) {
+
+      if (i+1 == argc) {
+        fputs("\t[\033[31mERROR\033[0m] Could not find hardware address in command line argument\n", stderr);
+        exit(1);
+      }
+      string_to_mac(argv[i+1], default_dest_mac);
+    }
+  }
+
+  if (0 == default_interface[0]) {
+    memcpy(default_interface, "eth0\0\0\0\0\0\0\0\0\0\0\0", IFNAMSIZ);
+  }
+}
 
 void print_mac(const unsigned char *mac_address, FILE *loc) {
   int i;
@@ -89,34 +121,28 @@ char char_to_hex(const char input) {
     case 'F':
       return '\xF';
     default:
-      fprintf(stderr, "\t[\033[31mERROR\033[0m] Unknown character in MAC address found: %c\n", input);
+      fprintf(stderr, "\t[\033[31mERROR\033[0m] Unknown character in hardware address found: %c\n", input);
       exit(1);
   } 
 }
 
 
 
-unsigned char *string_to_mac(const char *string) {
-  unsigned char *to_return = (unsigned char *) calloc(6, sizeof(*to_return));
-  if (NULL == to_return) {
-    fputs("\t[\033[31mERROR\033[0m] Failed to initialize mac address\n", stderr);
-    exit(1);
-  }
-  
+void string_to_mac(const char *string, unsigned char *dest_mac) {
+    
   int i, mac_bit = 0;
   for (i = 0; i < 17; i++) {
     if (!(i % 3)) {
-      to_return[mac_bit] = char_to_hex(string[i])*16;
+      dest_mac[mac_bit] = char_to_hex(string[i])*16;
     } else if (1 == (i % 3)) {
-      to_return[mac_bit++] += char_to_hex(string[i]);
+      dest_mac[mac_bit++] += char_to_hex(string[i]);
     }
   }
-  return to_return;
 }
 
 
 
-unsigned char *create_packet(const char *restrict destination_mac, struct ifreq source_mac, const char *restrict network_id, unsigned char *restrict mode, const char *restrict packet_id, uint_fast16_t *restrict tx_len) {
+unsigned char *create_packet(const char *restrict destination_mac, struct ifreq source_mac, const char *restrict network_id, unsigned char *restrict mode, const char *restrict packet_id) {
   unsigned char *sendbuffer = (unsigned char *) calloc(23, sizeof(*sendbuffer));
   if (NULL == sendbuffer) {
     fputs("\t[\033[31mERROR\033[0m] Failed to initialize send buffer\n", stderr);
@@ -124,41 +150,38 @@ unsigned char *create_packet(const char *restrict destination_mac, struct ifreq 
   }
 
   struct ether_header *eth = (struct ether_header *) sendbuffer;
-  strncpy(eth->ether_shost, (uint8_t *) &source_mac.ifr_hwaddr.sa_data, 6);
-  strncpy(eth->ether_dhost, destination_mac, 6);
+  memcpy(eth->ether_shost, (uint8_t *) &source_mac.ifr_hwaddr.sa_data, 6);
+  memcpy(eth->ether_dhost, destination_mac, 6);
   eth->ether_type = htons(HARDWARE_ETHERNET_LOCATION_PROTOCOL);
   
-  tx_len[0] += sizeof(struct ether_header);
-  int i;
+  sendbuffer += 14;
+  struct EtherPing *ether_ping = (struct EtherPing *) (sendbuffer);
 
-  for (i = 0; i < NETW_ID_LEN; i++) {
-    sendbuffer[tx_len[0]++] = network_id[i];
-  }
+  memcpy(ether_ping->network_id, network_id, NETW_ID_LEN);
   
-  if (!strncmp(destination_mac, "\xff\xff\xff\xff\xff\xff", 6)) {
-    sendbuffer[tx_len[0]++] = MODE_BROADSEND;
+  if (!memcmp(destination_mac, "\xff\xff\xff\xff\xff\xff", 6)) {
+    ether_ping->mode = MODE_BROADSEND;
     mode[0] = MODE_BROADREPLY;
   } else {
-    sendbuffer[tx_len[0]++] = MODE_SEND;
+    ether_ping->mode = MODE_SEND;
     mode[0] = MODE_REPLY;
   }
 
-  for (i = 0; i < PKG_ID_LEN; i++) {
-    sendbuffer[tx_len[0]++] = packet_id[i];
-  }
-
+  memcpy(ether_ping->packet_id, packet_id, PKT_ID_LEN);
+  
+  sendbuffer -= 14;
   return sendbuffer;
 }
 
 
 
-void send_packet(const int socket_fd, const unsigned char *packet, const uint_fast16_t *tx_len, const unsigned char *destination_mac, struct ifreq interface_ID) {
+void send_packet(const int socket_fd, const unsigned char *packet, const unsigned char *destination_mac, struct ifreq interface_ID) {
   struct sockaddr_ll socket_address;
   socket_address.sll_ifindex = interface_ID.ifr_ifindex;
   socket_address.sll_halen = ETH_ALEN;
 
-  strncpy(socket_address.sll_addr, destination_mac, 6);
-  if (sendto(socket_fd, packet, tx_len[0], 0, (const struct sockaddr *) &socket_address, sizeof(struct sockaddr_ll)) < 0) {
+  memcpy(socket_address.sll_addr, destination_mac, 6);
+  if (sendto(socket_fd, packet, 21, 0, (const struct sockaddr *) &socket_address, sizeof(struct sockaddr_ll)) < 0) {
     puts("\t[\033[31;1mCRITICAL\033[0m] Failed to send packet, please check your network connection");
     exit(1);
   } else {
@@ -172,12 +195,12 @@ void *wait_for_packet(void *thread_argument) {
   /* this function waits three seconds, prints out that the host is unavailable and quits the program */
   unsigned char *mac_address = (unsigned char *) calloc(6, sizeof(*mac_address));
   if (NULL == mac_address) {
-    fputs("\t[\033[31mERROR\033[0m] Failed to initialize mac address display buffer\n", stderr);
+    fputs("\t[\033[31mERROR\033[0m] Failed to initialize hardware address display buffer\n", stderr);
     exit(1);
   }
-  strncpy(mac_address, (char *) thread_argument, 6);
+  memcpy(mac_address, (char *) thread_argument, 6);
   sleep(1);
-  if (strncmp(mac_address, "\xff\xff\xff\xff\xff\xff", 6)) {
+  if (memcmp(mac_address, "\xff\xff\xff\xff\xff\xff", 6)) {
     fputs("\t[\033[31mERROR\033[0m] Failed to reach host ", stderr);print_mac(mac_address, stderr);fputs(" (Timeout), please check your ethernet configuration\n", stderr);
   }
   exit(0);
@@ -188,17 +211,28 @@ void *wait_for_packet(void *thread_argument) {
 
 int main(int argc, char *argv[]) {
   if (argc < 2) {
-    puts("Usage: ./ping2 [\033[32;1mMAC_ADDRESS\033[0m]");
+    puts("\nRequired:\n-m/--mac\t\t\tHW_ADDRESS\n\nOptionally:\n-i/--interface\t\t\tINTERFACE\n\nDefault configuration:\nINTERFACE=\"eth0\"\nCOUNT=1\n");
     exit(1);
   }
   int i;
-
   pthread_t pkt_timeout;
-  unsigned char *destination_mac = string_to_mac(argv[1]);
+
+  unsigned char *destination_mac = (unsigned char *) calloc(6, sizeof(*destination_mac));
+  if (NULL == destination_mac) {
+    fputs("\t[\033[31mERROR\033[0m] Failed to initialize hardware address register\n", stderr);
+    exit(1);
+  }
+
+  char *interface_name = (char *) calloc(IFNAMSIZ, sizeof(*interface_name));
+  if (NULL == interface_name) {
+    fputs("\t[\033[31mERROR\033[0m] Failed to initialize interface name register\n", stderr);
+    exit(1);
+  }
+
 
   unsigned char *read_buffer = (unsigned char *) calloc(64, sizeof(*read_buffer));
   if (NULL == read_buffer) {
-    fputs("\t[\033[31mERROR\033[0m] Failed to initialize read buffer\n", stderr);
+    fputs("\t[\033[31mERROR\033[0m] Failed to initialize receiving register\n", stderr);
     exit(1);
   }
 
@@ -207,20 +241,12 @@ int main(int argc, char *argv[]) {
     fputs("\t[\033[31mERROR\033[0m] Failed to initialize socket object\n", stderr);
     exit(1);
   }
-  
-  uint_fast16_t *transmission_length = (uint_fast16_t *) calloc(MODE_LEN, sizeof(*transmission_length));
-  if (NULL == transmission_length) {
-    fputs("\t[\033[31mERROR\033[0m] Failed to initialize transmission length register\n", stderr);
-    exit(1);
-  }
-  transmission_length[0] = 0;
 
   char *network_id = (char *) calloc(NETW_ID_LEN, sizeof(*network_id));
   if (NULL == network_id) {
     fputs("\t[\033[31mERROR\033[0m] Failed to initialize network ID register\n", stderr);
     exit(1);
   }
-  /* here, the program would, in the future, read the ID from a config */
  
   unsigned char *mode = (unsigned char *) calloc(1, sizeof(*mode));
   if (NULL == mode) {
@@ -228,28 +254,27 @@ int main(int argc, char *argv[]) {
     exit(1);
   }
 
-  char *packet_id = (char *) calloc(PKG_ID_LEN, sizeof(*packet_id));
+  char *packet_id = (char *) calloc(PKT_ID_LEN, sizeof(*packet_id));
   if (NULL == packet_id) {
     fputs("\t[\033[31mERROR\033[0m] Failed to initialize packet ID register\n", stderr);
     exit(1);
-  };
+  }
 
   struct ifreq interface_ID, interface_MAC;
-
-  char interface_name[IFNAMSIZ];
-  strncpy(interface_name, DEF_IF, IFNAMSIZ);
+  
+  process_arguments(argc, argv, interface_name, destination_mac);
 
   memset(&interface_ID, 0, sizeof(struct ifreq));
   memset(&interface_MAC, 0, sizeof(struct ifreq));
 
-  strncpy(interface_ID.ifr_name, interface_name, IFNAMSIZ-1);
-  strncpy(interface_MAC.ifr_name, interface_name, IFNAMSIZ-1);
+  memcpy(interface_ID.ifr_name, interface_name, IFNAMSIZ-1);
+  memcpy(interface_MAC.ifr_name, interface_name, IFNAMSIZ-1);
   if (ioctl(socket_fd, SIOCGIFINDEX, &interface_ID) < 0) {
-    fputs("\t[\033[31mERROR\033[0m] Failed to bind network interface to socket\n", stderr);
+    fprintf(stderr, "\t[\033[31mERROR\033[0m] Failed to bind network interface \"%s\" to socket\n", interface_name);
     exit(1);
   }
   if (ioctl(socket_fd, SIOCGIFHWADDR, &interface_MAC) < 0) {
-    fputs("\t[\033[31mERROR\033[0m] Failed to bind network MAC address to socket\n", stderr);
+    fputs("\t[\033[31mERROR\033[0m] Failed to bind network hardware address to socket\n", stderr);
     exit(1);
   }
   
@@ -260,16 +285,16 @@ int main(int argc, char *argv[]) {
     exit(1);
   }
  
-  unsigned char *transmission_packet = create_packet(destination_mac, interface_MAC, network_id, mode, packet_id, transmission_length);
-  send_packet(socket_fd, transmission_packet, transmission_length, destination_mac, interface_ID);
+  unsigned char *transmission_packet = create_packet(destination_mac, interface_MAC, network_id, mode, packet_id);
+  send_packet(socket_fd, transmission_packet, destination_mac, interface_ID);
   pthread_create(&pkt_timeout, NULL, wait_for_packet, destination_mac);
 
   while (1) {
     recvfrom(socket_fd, read_buffer, 64, 0, NULL, NULL);
-    if (strncmp(rec_eh->ether_dhost, (uint8_t *) &interface_MAC.ifr_hwaddr.sa_data, 6) && strncmp(rec_eh->ether_dhost, "\xff\xff\xff\xff\xff\xff", 6)) {
+    if (memcmp(rec_eh->ether_dhost, (uint8_t *) &interface_MAC.ifr_hwaddr.sa_data, 6) && memcmp(rec_eh->ether_dhost, "\xff\xff\xff\xff\xff\xff", 6)) {
       continue;
     }
-    strncpy(packet_sender, rec_eh->ether_shost, 6);
+    memcpy(packet_sender, rec_eh->ether_shost, 6);
      
     if (rec_eh->ether_type != htons(HARDWARE_ETHERNET_LOCATION_PROTOCOL)) {
       continue;
@@ -277,7 +302,7 @@ int main(int argc, char *argv[]) {
 
     read_buffer += 14;
 
-    if (strncmp(read_buffer, network_id, NETW_ID_LEN)) {
+    if (memcmp(read_buffer, network_id, NETW_ID_LEN)) {
       continue;
     }
     read_buffer += NETW_ID_LEN;
@@ -287,7 +312,7 @@ int main(int argc, char *argv[]) {
     }
 
     read_buffer += MODE_LEN;
-    if (!strncmp(read_buffer, packet_id, PKG_ID_LEN)) {
+    if (!memcmp(read_buffer, packet_id, PKT_ID_LEN)) {
       printf("\t[\033[32;1mSUCCESS\033[0m] Successfully located host ");print_mac(packet_sender, stdout);putchar('\n');
       if (MODE_BROADREPLY != mode[0]) {
         return 0;
